a82bf8445eca6b5161d2d68cacc12e991fe26463
shailesh <shaileshbassi8@gmail.com>
2024-05-14 23:00:43 +0530

    testing the lang value change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    // external dependencies
-  </script>
-  
-  <svelte:head>
-    <title>Shailesh</title>
-    <meta name="description" content="Shailesh | Svelte Demo" />
-  </svelte:head>
+  // external dependencies
+</script>
+
+<svelte:head>
+  <title>Shailesh</title>
+  <meta name="description" content="Shailesh | Svelte Demo" />
+</svelte:head>
