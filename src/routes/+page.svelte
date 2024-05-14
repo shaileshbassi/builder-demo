41bd1a28b96d5e1b3a49bdca4ecd3a41776a0f80
shailesh <shaileshbassi8@gmail.com>
2024-05-14 22:38:22 +0530

    adding page svelte file

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    // external dependencies
+  </script>
+  
+  <svelte:head>
+    <title>Shailesh</title>
+    <meta name="description" content="Shailesh | Svelte Demo" />
+  </svelte:head>
